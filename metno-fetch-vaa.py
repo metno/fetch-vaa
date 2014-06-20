4b27001e37ec5037cc521f901bb9af41f0e69b4b
David Boddie <david.boddie@met.no>
2014-06-20 18:07:26 +0200

    Converted the tool to a GUI tool that can select different VAA centres.

--- metno-fetch-vaa.py
+++ metno-fetch-vaa.py
@@ -16,9 +16,45 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import commands, HTMLParser, os, sys, urllib2, urlparse
+import commands, datetime, HTMLParser, os, subprocess, sys, urllib2, urlparse
 
-default_url = "http://www.meteo.fr/vaac/evaa.html"
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+class Settings(QSettings):
+
+    """Convenience class to help read values from settings files as Python datatypes.
+    """
+
+    def __init__(self, organisation, product):
+
+        QSettings.__init__(self, organisation, product)
+    
+    def value(self, key, default = QVariant()):
+    
+        """Reads the value from the settings file that corresponds to the given key,
+        with the type defined by the default value. If the key is not defined in the
+        settings file then the default value is returned instead.
+        """
+        value = QSettings.value(self, key, default)
+        if type(default) == int:
+            return value.toInt()[0]
+        elif type(default) == bool:
+            return value.toBool()
+        elif type(default) == float:
+            return value.toFloat()[0]
+        elif type(default) == str:
+            return str(value.toString())
+        elif type(default) == unicode:
+            return unicode(value.toString())
+        elif type(default) == list:
+            if type(default[0]) == int:
+                return map(lambda v: v.toInt()[0], value.toList())
+        else:
+            return value
+        
+        raise ValueError, "Unknown value type for key '%s': %s" % (key, type(default))
+
 
 class Parser(HTMLParser.HTMLParser):
 
@@ -27,6 +63,8 @@ class Parser(HTMLParser.HTMLParser):
         HTMLParser.HTMLParser.__init__(self)
         self.href = ""
         self.text = ""
+        self.table_row = []
+        self.tags = []
     
     def feed(self, data):
 
@@ -35,99 +73,269 @@ class Parser(HTMLParser.HTMLParser):
     
     def handle_starttag(self, tag, attrs):
     
+        self.tags.append(tag)
+        
         if tag == "a":
             d = dict(attrs)
             try:
                 self.href = d["href"]
             except KeyError:
                 pass
-            self.text = ""
+        elif tag == "tr":
+            self.table_row = []
+        
+        self.text = ""
 
     def handle_data(self, data):
     
-        if self.href:
-            self.text += data
+        self.text += data
     
     def handle_endtag(self, tag):
 
         if tag == "a":
-            self.anchors.append((self.href, self.text))
+            self.anchors.append((self.href, self.text, self.table_row))
             self.href = ""
+        elif tag == "td":
+            self.table_row.append(self.text)
+        
+        # Discard any non-matching end tags.
+        while self.tags:
+            if self.tags.pop() == tag:
+                break
+
+
+class ToulouseFetcher:
+
+    url = "http://www.meteo.fr/vaac/evaa.html"
+    number_to_fetch = 10
+
+    def fetch(self, vaaList):
+    
+        "Reads the messages available from the URL for the current VAA centre."
+
+        html = urllib2.urlopen(self.url).read()
+        p = Parser()
+        p.feed(html)
+        p.close()
+        
+        count = 0
+
+        for href, text, table_text in p.anchors:
+            if text.endswith("UTC"):
+                # The date is encoded in the URL for the advisory.
+                info = href.split(".")
+                date = datetime.datetime.strptime(info[-2], "%Y%m%d%H%M").strftime("%Y-%m-%d %H:%M")
+                volcano = info[2].replace("_", " ")
+                item = QListWidgetItem("%s (%s)" % (date, volcano))
+                item.href = href
+                item.url = urlparse.urljoin(self.url, href)
+                vaaList.addItem(item)
+
+                count += 1
+                if count == self.number_to_fetch:
+                    break
+
+
+class AnchorageFetcher:
+
+    url = "http://vaac.arh.noaa.gov/list_vaas.php"
+    number_to_fetch = 10
+
+    def fetch(self, vaaList):
+    
+        "Reads the messages available from the URL for the current VAA centre."
+
+        html = urllib2.urlopen(self.url).read()
+        p = Parser()
+        p.feed(html)
+        p.close()
+        
+        count = 0
+
+        for href, text, table_text in p.anchors:
+            if text == "X" and href.split("/")[-2] == "VAA":
+                # The date is encoded in the associated table text.
+                date = datetime.datetime.strptime(table_text[0], "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d %H:%M:%S")
+                volcano = table_text[1].replace("_", " ")
+                item = QListWidgetItem("%s (%s)" % (date, volcano))
+                item.href = href
+                item.url = urlparse.urljoin(self.url, href)
+                vaaList.addItem(item)
+
+                count += 1
+                if count == self.number_to_fetch:
+                    break
+
+
+class Window(QMainWindow):
+
+    def __init__(self, fetchers):
+
+        QMainWindow.__init__(self)
+
+        self.fetchers = fetchers
+        self.settings = Settings("met.no", "metno-fetch-vaa")
+        
+        contentWidget = QWidget()
+        layout = QGridLayout(contentWidget)
+        
+        # Create a list of available VAA centres.
+        urlLabel = QLabel(self.tr("&Advisory Centres"))
+        self.urlList = QListWidget()
+        urlLabel.setBuddy(self.urlList)
+        layout.addWidget(urlLabel, 0, 0)
+        layout.addWidget(self.urlList, 1, 0)
+
+        self.fetchButton = QPushButton(self.tr("&Fetch messages"))
+        self.fetchButton.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Fixed)
+        layout.addWidget(self.fetchButton, 2, 0)
+        layout.setAlignment(self.fetchButton, Qt.AlignHCenter)
+
+        # Create a list of downloaded advisories.
+        vaaLabel = QLabel(self.tr("&Messages"))
+        self.vaaList = QListWidget()
+        vaaLabel.setBuddy(self.vaaList)
+        layout.addWidget(vaaLabel, 0, 1)
+        layout.addWidget(self.vaaList, 1, 1)
+        
+        self.convertButton = QPushButton(self.tr("&Convert messages"))
+        self.convertButton.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Fixed)
+        layout.addWidget(self.convertButton, 2, 1)
+        layout.setAlignment(self.convertButton, Qt.AlignHCenter)
+        
+        # Add a work log.
+        self.workLog = QPlainTextEdit()
+        self.workLog.setReadOnly(True)
+        layout.addWidget(self.workLog, 3, 0, 1, 3)
+        layout.setRowStretch(1, 1)
+
+        # Make connections.
+        self.urlList.itemSelectionChanged.connect(self.updateButtons)
+        self.fetchButton.clicked.connect(self.fetchAdvisories)
+        self.convertButton.clicked.connect(self.convertAdvisories)
+        
+        # Fetch the list of VAA centres.
+        self.fetchVAACList()
+        
+        self.updateButtons()
+
+        self.setCentralWidget(contentWidget)
+        self.setWindowTitle(self.tr("Fetch Volcanic Ash Advisories"))
+        self.restoreGeometry(self.settings.value("window/geometry").toByteArray())
+    
+    def fetchVAACList(self):
+    
+        names = self.fetchers.keys()
+        names.sort()
+
+        for name in names:
+
+            item = QListWidgetItem(name)
+            item.name = name
+            self.urlList.addItem(item)
+    
+    def fetchAdvisories(self):
+    
+        QApplication.setOverrideCursor(Qt.WaitCursor)
+        
+        self.vaaList.clear()
+        item = self.urlList.currentItem()
+        fetcher = self.fetchers[item.name]
+        fetcher.fetch(self.vaaList)
+        self.updateButtons()
+        
+        QApplication.restoreOverrideCursor()
+
+    def updateButtons(self):
+    
+        self.fetchButton.setEnabled(self.urlList.currentItem() != None)
+        self.convertButton.setEnabled(self.vaaList.count() > 0)
+    
+    def convertAdvisories(self):
+    
+        QApplication.setOverrideCursor(Qt.WaitCursor)
+        
+        output_dir = self.settings.value("work directory", os.path.join(os.getenv("HOME"), ".diana", "work"))
+        
+        kml_files = []
+        failed_files = []
+        self.workLog.clear()
+        
+        for i in range(self.vaaList.count()):
+        
+            item = self.vaaList.item(i)
+            href = item.href
+            url = item.url
+            
+            file_name = href.split("/")[-1]
+            
+            vaa_file = os.path.join(output_dir, file_name)
+            if vaa_file.endswith(".html"):
+                kml_file = os.path.join(output_dir, file_name.replace(".html", ".kml"))
+            else:
+                kml_file = file_name + ".kml"
+            
+            # Do not download the file if we already have it.
+            if not os.path.exists(kml_file):
+                vaa_content = urllib2.urlopen(url).read()
+
+                # Wrap any non-HTML content in a <pre> element.
+                if not vaa_file.endswith(".html"):
+                    vaa_content = "<pre>\n" + vaa_content + "\n</pre>\n"
+
+                open(vaa_file, "w").write(vaa_content)
+            else:
+                # If we already have the file then we should also have the
+                # files that were published before it and which follow it
+                # in the list.
+                break
+            
+            # Convert the message in the HTML file to a KML file.
+            s = subprocess.Popen(["/usr/bin/metno-vaa-kml", vaa_file],
+                                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            if s.wait() != 0:
+                failed_files.append(vaa_file)
+            else:
+                # Remove the HTML file.
+                os.remove(vaa_file)
+                kml_files.append(kml_file)
+
+            self.workLog.setPlainText(self.workLog.toPlainText() + s.stdout.read())
+            QApplication.processEvents()
+        
+        text = ""
+        
+        count = len(kml_files)
+        if count == 0:
+            text = self.tr("No new KML files available.\n")
+        elif count == 1:
+            text = self.tr("1 new KML file can be found in the '%1' directory.\n").arg(output_dir)
+        else:
+            text = self.tr("%1 new KML files can be found in the '%2' directory.\n").arg(count).arg(output_dir)
+        
+        count = len(failed_files)
+        if count == 1:
+            text += self.tr("1 unprocessed VAA file can be found in the '%1' directory.").arg(output_dir)
+        else:
+            text += self.tr("%1 unprocessed VAA files can be found in the '%2' directory.").arg(count).arg(output_dir)
+        
+        self.workLog.setPlainText(self.workLog.toPlainText() + text)
+        
+        QApplication.restoreOverrideCursor()
+    
+    def closeEvent(self, event):
+
+        self.settings.setValue("window/geometry", self.saveGeometry())
+        self.settings.sync()
 
 
 if __name__ == "__main__":
 
-    args = sys.argv[:]
-    gui = "--gui" in args
-    if gui:
-        args.remove("--gui")
-    
-    if len(args) == 2:
-        output_dir = args[1]
-    elif len(args) == 1:
-        output_dir = os.path.join(os.getenv("HOME"), ".diana", "work")
-    else:
-        sys.stderr.write("Usage: %s [--gui] [download directory]\n" % args[0])
-        sys.exit(1)
-    
-    if not os.path.isdir(output_dir):
-        text = "Download directory '%s' not found." % output_dir
-        if gui:
-            os.system('zenity --error --text="%s"' % text)
-        else:
-            sys.stderr.write(text + "\n")
-        sys.exit(1)
-    
-    url = default_url
-    
-    html = urllib2.urlopen(url).read()
-    p = Parser()
-    p.feed(html)
-    p.close()
-    
-    count = 0
-    for href, text in p.anchors:
-        if text.endswith("UTC"):
-            vaa_url = urlparse.urljoin(url, href)
-            file_name = href.split("/")[-1]
+    app = QApplication(sys.argv)
 
-            # Process the file name to get the embedded time information.
-            # This is currently unused.
-            pieces = file_name.split(".")
-            time_str = pieces[-2]
-            
-            vaa_file = os.path.join(output_dir, file_name)
-            kml_file = os.path.join(output_dir, file_name.replace(".html", ".kml"))
+    fetchers = {u"Toulouse VAAC": ToulouseFetcher(),
+                u"Anchorage VAAC": AnchorageFetcher()}
 
-            # Do not download the file if we already have it.
-            if not os.path.exists(kml_file):
-                vaa_html = urllib2.urlopen(vaa_url).read()
-                open(vaa_file, "w").write(vaa_html)
-            else:
-                # If we already have the file then we should also have the
-                # files that preceded it.
-                break
-            
-            # Convert the message in the HTML file to a KML file.
-            os.system("metno-vaa-kml " + commands.mkarg(vaa_file))
-
-            # Remove the HTML file.
-            os.remove(vaa_file)
-            
-            count += 1
-            if count == 10:
-                break
-    
-    if count == 0:
-        text = "No new KML files retrieved."
-    elif count == 1:
-        text = "1 new KML file can be found in the '%s' directory." % output_dir
-    else:
-        text = "%i new KML files can be found in the '%s' directory." % (count, output_dir)
-    
-    if gui:
-        os.system('zenity --info --text="%s"' % text)
-    else:
-        print text
-
-    sys.exit()
+    window = Window(fetchers)
+    window.show()
+    sys.exit(app.exec_())
