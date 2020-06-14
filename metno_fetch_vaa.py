f544a06b9203dab57045393cd557799d3047c1f6
Trond Michelsen <trondm@met.no>
2020-06-15 01:28:13 +0200

    version bump

--- metno_fetch_vaa.py
+++ metno_fetch_vaa.py
@@ -24,7 +24,7 @@ import PyQt5.QtWidgets as QtWidgets
 import webbrowser
 import re
 
-__version__ = "0.9.13"
+__version__ = "0.9.14"
 
 checked_dict = {False: QtCore.Qt.Unchecked, True: QtCore.Qt.Checked}
 
