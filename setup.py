48ddcb320e7703bef1b2ce54858bbb9a3c85b695
Trond Michelsen <trondm@met.no>
2020-06-15 00:51:16 +0200

    Moved metno-vaa-kml into project

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     url="http://www.met.no/",
     version=metno_fetch_vaa.__version__,
     py_modules=["metno_fetch_vaa", "selectVaac"],
-    scripts=["fetch_vaac.py", "metno-fetch-vaa.py"],
+    scripts=["fetch_vaac.py", "metno-fetch-vaa.py", "metno-vaa-kml"],
     data_files=[("share/applications", ["share/applications/metno-fetch-vaa.desktop"]),
-                ("share/pixmaps", ["share/pixmaps/metno-fetch-vaa.png"])]
+                ("share/pixmaps", ["share/pixmaps/metno-fetch-vaa.png"]),
+                ("share/metno-vaa-kml", ["share/metno-vaa-kml/volcano.tt", "share/metno-vaa-kml/volcano.grammar"])]
     )
